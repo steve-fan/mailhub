37a60de1a258e4534c7fe700b444ada6f164c6bb
fanzhenchao <fanzc.daily@gmail.com>
2016-12-09 15:09:04 +0800

    pass vendor client as initialize params

--- lib/mailhub/hub.rb
+++ lib/mailhub/hub.rb
@@ -5,26 +5,20 @@ module Mailhub
   class Hub
     attr_accessor :settings
 
-    # TODO: pass vendor as parameters
     def initialize(settings)
       @settings = settings
-      @postmark_client = ::Postmark::ApiClient.new(settings[:postmark_api_token])
     end
 
     def deliver!(mail)
       vendors.each do |vendor|
-        send(:"deliver_by_#{vendor}", mail)
+        vendor.deliver_message(mail)
         break if mail.delivered?
       end
       mail
     end
 
     def vendors
-      [:postmark]
-    end
-
-    def deliver_by_postmark(mail)
-      @postmark_client.deliver_message(mail)
+      settings[:vendors]
     end
   end
 end
