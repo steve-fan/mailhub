9498decef7775c36d90af114049be5ba8fb26046
fanzhenchao <fanzc.daily@gmail.com>
2016-12-09 14:51:31 +0800

    fixed test

--- spec/hub_spec.rb
+++ spec/hub_spec.rb
@@ -1,13 +1,4 @@
 require 'spec_helper'
 
 describe Mailhub::Hub do
-  let(:settings) do
-    {
-      postmark_api_token: 'new-api-token-value',
-    }
-  end
-
-  it 'has one vendors' do
-    expect(Mailhub::Hub.vendors).to eq([:postmark])
-  end
 end
